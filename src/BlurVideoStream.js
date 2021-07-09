7b4ce6f0c90073d9395bf5804d4ca2fa1dc924e4
mann patel <mannpatel1360@gmail.com>
2021-07-09 11:27:33 +0530

    finally all done

--- src/BlurVideoStream.js
+++ src/BlurVideoStream.js
@@ -1,9 +1,10 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef
+} from 'react';
 import * as tf from '@tensorflow/tfjs';
 import * as bodyPix from '@tensorflow-models/body-pix';
-import { Button } from 'react-bootstrap';
-const BlurVideoStream= () => {
-  
+
+const BlurVideoStream = () => {
+
   const [temp, setTemp] = useState(true);
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
@@ -36,7 +37,7 @@ const BlurVideoStream= () => {
 
     bodyPix.load(options)
       .then(net => perform(net))
-      .catch(err => console.log("body pix error"+err))
+      .catch(err => console.log("body pix error" + err))
   };
 
 
@@ -70,29 +71,41 @@ const BlurVideoStream= () => {
 
   function stopVideoStream() {
     const stream = videoRef.current.srcObject;
-    if(temp){
+    if (temp) {
       stream.getTracks().forEach(track => track.stop());
-      if(videoRef.current.srcObject){videoRef.current.srcObject = null}
+      if (videoRef.current.srcObject) { videoRef.current.srcObject = null }
     }
   }
 
-return (
+  return (
     <>
-      <div style={{ textAlign: 'center', margin: '10px' }}>Real Video
-        <video id="video" ref={videoRef} width="480" height="320" autoPlay={true} playsInline></video>
-      </div>
+    
+    
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark justify-content-center">
+        <a className="navbar-brand" href="#"></a>
+        <ul className="navbar-nav">
+          <h2>Bluring Image Using TensorFlow BodyPix</h2>
+        </ul>
+      </nav><br/>
 
-      <div style={{ textAlign: 'center' }}>Blur Video<canvas id="canvas" width="480" height="320" ref={canvasRef}></canvas>
+    
+      <div className="row ">
+        <div className="col-12 col-sm-6 border justify-content-center text-center">
+          <h2>Real Video Stream</h2>
+          <video id="video" ref={videoRef} width="480" height="420" autoPlay={true} playsInline></video>
+        </div>
+        <div className="col-12 col-sm-6 border text-center">
+          <h2>Blur Video Stream</h2>
+          <canvas id="canvas" width="480" height="420" ref={canvasRef} playsInline></canvas>
+        </div>
+        <div className="col-12 col-sm-12 text-center">
+          <button id="start-btn"className="btn btn-success m-2 " type="button" onClick={startVideo} >Start</button>
+          <button id="stop-btn" className="btn btn-danger m-2" type="button" onClick={stopVideo} >Stop</button>
+        </div>
       </div>
-
-      <div style={{ textAlign: 'center' }}>
-        <button id="start-btn" type="button" onClick={startVideo} >Start</button>
-        <button id="stop-btn" type="button" onClick={stopVideo} >Stop</button>
-      </div>
+      
     </>
   );
-
-
 }
 
 export default BlurVideoStream;
